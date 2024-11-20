3d25b3c3f7e79e1a6d9c102967e0e13413880460
LazariaInk <lorienlored@gmail.com>
2024-11-20 18:03:14 +0300

    init spring empty project

---
+++ src/main/java/com/pluriverse/user_status_service/UserStatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pluriverse.user_status_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserStatusServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluriverse/user_status_service/UserStatusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluriverse.user_status_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserStatusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
